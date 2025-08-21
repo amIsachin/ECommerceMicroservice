0305a733faa195044c5c1d8c13fe7a4dcd6d6e77
amIsachin <amIsachin@hotmail.com>
2025-08-21 23:13:19 +0530

    Added api gateway ocelot

---
+++ Customer.Presentation/Controllers/CustomerController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Customer.Presentation.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Hello");
+
+        }
+    }
+}
